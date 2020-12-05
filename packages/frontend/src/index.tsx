3e39ea66143c521333456d8afffa26c8632a296b
Bulent <eroglu.bulent98@gmail.com>
2020-12-05 19:21:47 +0000

    react app init

---
+++ packages/frontend/src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+
+render(<div>hi</div>, document.getElementById('root'));
---
+++ packages/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.jsx', './public/index.html'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
